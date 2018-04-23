a2894dac3d7fcef78f85f80c6d11d926755b4c4f
fengshiyou <501453944@qq.com>
2018-04-23 18:58:02 +0800

    跨域请求的中间件

---
+++ app/Http/Middleware/CrossHttp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CrossHttp
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //@todo  跨域相关 需要研究一下什么情况
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: *");
+        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
+        header("Access-Control-Expose-Headers: *");
+
+        return $next($request);
+    }
+}
