b5ca8bf70b067d106fcf33adc9bd8a302af57297
fengshiyou <501453944@qq.com>
2018-06-26 17:21:29 +0800

    用户相关API

--- app/Http/Controllers/GithubHookController.php
+++ app/Http/Controllers/GithubHookController.php
@@ -32,7 +32,7 @@ class GithubHookController extends Controller
     }
     public function make_apidoc(){
         $path = ' /data/www/api-popo-blog';
-        $passwd = 'Ace___7';
+        $passwd = 'Ace~~~~7';
         $shell_command = "cd $path && echo '$passwd' | /usr/bin/sudo -S sh apidoc.sh ";
         shell_exec($shell_command);
         var_dump($shell_command);
